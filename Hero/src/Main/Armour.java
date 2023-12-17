31c357582e60027e0fb72d16ac6effbd99736b2f
Ivan <ivan_koyrushki@abv.bg>
2023-12-17 19:33:52 +0200

    Fix

--- Hero/src/Main/Armour.java
+++ Hero/src/Main/Armour.java
@@ -5,6 +5,14 @@ public class Armour extends Item {
 	
 	public Armour(String name,int defense) {
 		super(name);
+		this.setDefense(defense);
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
 		this.defense = defense;
 	}
 
